68ddff58d634fee59532b9d93fb1971caa4f159a
Faith Kariuki <tcwamboi@gmail.com>
2024-07-11 18:20:36 +0300

    Initialize project with custom User and Organization models

---
+++ accounts/models.py
@@ -0,0 +1,55 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+
+
+class Organisation(models.Model):
+    orgId = models.CharField(max_length=36, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, firstName, lastName, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, firstName=firstName, lastName=lastName, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        # Create an organization for the user
+        orgName = f"{firstName}'s Organisation"
+        Organisation.objects.create(name=orgName, description="Default organisation", users=[user])
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    userId = models.CharField(max_length=36, default=uuid.uuid4, editable=False, unique=True)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    organisations = models.ManyToManyField(Organisation, related_name='users')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['firstName', 'lastName']
+
+    def __str__(self):
+        return self.email
