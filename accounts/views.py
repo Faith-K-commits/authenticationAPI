6172c7e3ae94e9ea0a3fe9646c59a0b3ef7937db
Faith Kariuki <tcwamboi@gmail.com>
2024-07-24 18:38:44 +0300

    Final commit

--- accounts/views.py
+++ accounts/views.py
@@ -33,13 +33,19 @@ def register_user(request):
 
 @api_view(['POST'])
 def login_user(request):
+    # Extract email and password from the request data
     email = request.data.get('email')
     password = request.data.get('password')
     try:
+        # Attempt to retrieve the user by email
         user = User.objects.get(email=email)
+        # Check  if the password provided matches the stored password for the user
         if not user.check_password(password):
+            # Raise an exception if the password is incorrect
             raise ValueError("Invalid password")
+        # Generate a new refresh token for the user
         refresh = RefreshToken.for_user(user)
+        # Return a successful response with the access token and user details
         return Response({
             "status": "success",
             "message": "Login successful",
@@ -54,6 +60,7 @@ def login_user(request):
                 }
             }
         }, status=status.HTTP_200_OK)
+    # Handle case where no user is found by the provided email
     except User.DoesNotExist:
         return Response({
             "status": "Bad request",
@@ -104,10 +111,13 @@ def get_user_record(request, user_id):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_user_organisations(request):
+    # Retrieve currently authenticated user from the request
     user = request.user
+    # Get all orgs associated wu=ith the user
     organisations = user.organisations.all()
     serializer = OrganisationSerializer(organisations, many=True)
 
+    # Return a successful response with serialized organisation data
     return Response({
         "status": "success",
         "message": "Organisations retrieved successfully",
@@ -120,8 +130,11 @@ def get_user_organisations(request):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_organisation_record(request, org_id):
+    # Retrieve the organisation object using the provided org_id, or return a 404 error if not found
     organisation = get_object_or_404(Organisation, orgId=org_id)
+    # Check if the authenticated user is associated with the organisation
     if request.user.organisations.filter(orgId=org_id).exists():
+        # Serialize the organisation data
         serializer = OrganisationSerializer(organisation)
         return Response({
             'status': 'success',
@@ -135,12 +148,17 @@ def get_organisation_record(request, org_id):
         }, status=403)
 
 
+# Only accepts POST requests and requires the user to be authenticated
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def create_organisation(request):
+    # Initialize serializer with the data received in the request
     serializer = OrganisationSerializer(data=request.data)
+    # Check if data provided is valid according to the serializer
     if serializer.is_valid():
+        # Save the valid data to create a new organisation instance
         serializer.save()
+        #  Return a successful response with the created organisation's data
         return Response({
             'status': 'success',
             'message': 'Organisation created successfully',
